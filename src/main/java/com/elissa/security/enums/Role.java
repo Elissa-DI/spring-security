8488a1df4188a3e4a3762a2cd7e0e0256e16a0b8
Elissa-DI <elissadusabe@gmail.com>
2025-07-22 12:19:45 +0200

    feat: Spring-security with Jwt-auth & role-based access control

---
+++ src/main/java/com/elissa/security/enums/Role.java
@@ -0,0 +1,6 @@
+package com.elissa.security.enums;
+
+public enum Role {
+    USER,
+    ADMIN
+}
---
+++ src/test/java/com/elissa/security/SecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elissa.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
